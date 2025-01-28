b6e6dc16cc606c19f5225abb6b67ad162ae90c21
OpenCo <admin@openco.ca>
2025-01-28 09:46:28 -0100

    Update smart_text import to use smart_str for compatibility

--- django_startr/admin.py
+++ django_startr/admin.py
@@ -1,6 +1,7 @@
 from django.core.validators import URLValidator
 from django.core.exceptions import FieldDoesNotExist
-from django.utils.encoding import smart_text
+#from django.utils.encoding import smart_text
+from django.utils.encoding import smart_str as smart_text
 from django.utils.safestring import mark_safe
 
 from functools import partial
